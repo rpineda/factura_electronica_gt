cef24d3d7d4f022e940b127d589d70b83da3c17f
Ricardo Pineda <pinedasoft@gmail.com>
2021-05-08 20:51:39 -0600

    FIX Sender dat_company field not exist

--- factura_electronica/__init__.py
+++ factura_electronica/__init__.py
@@ -11,7 +11,7 @@ from numinwords import numinwords
 
 # locale.setlocale(locale.LC_ALL, str('en_US.UTF-8'))
 
-__version__ = '7.0.908'
+__version__ = '7.0.909'
 
 @frappe.whitelist()
 def currency_in_words(amount, currency, cent_in_numb=0):
